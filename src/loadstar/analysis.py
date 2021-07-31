1dd02c64f82138e564b36df243b41f6c5f9fc8b0
Val Knight <val@valknight.xyz>
2021-07-31 23:14:04 +0100

    Add bounding box utility to loading analysis

--- src/loadstar/analysis.py
+++ src/loadstar/analysis.py
@@ -1,21 +1,29 @@
+from loadstar.util import BoundingBox
 
 PRINT_FAIL_COLOUR = False
 
 
-def checkIfLoading(grayVersion, pixelsToSkip: int, loadingColour: int, colourRange: int):
-    h, w = grayVersion.shape
+# Colours are in a range from 0 -> 255
+# We're fine to look from colours 0 -> 10
+
+
+def checkIfLoading(grayVersion, pixelsToSkip: int, loadingColour: int, colourRange: int, box: BoundingBox = None):
+    width, height = grayVersion.shape
+    if box is None:
+        box = BoundingBox((0,0),(width, height))
     loading = True
     skip = False
-    for x in range(0, h, pixelsToSkip):
+    for x in range(0, width, pixelsToSkip):
         if skip:
             break
-        for y in range(0, w, pixelsToSkip):
-            if grayVersion[x, y] > loadingColour + colourRange or grayVersion[x, y] < loadingColour - colourRange:
-                # ALL pixels must be loading for Cookstar.
-                loading = False
-                skip = True
-                if PRINT_FAIL_COLOUR:
-                    print("failed to verify loading at: {} (x,y : {},{})".format(
-                        grayVersion[x, y], x, y))
-                break
+        for y in range(0, height, pixelsToSkip):
+            if box.isInside(x,y):
+                if grayVersion[x, y] > loadingColour + colourRange or grayVersion[x, y] < loadingColour - colourRange:
+                    # ALL pixels must be loading for Cookstar.
+                    loading = False
+                    skip = True
+                    if PRINT_FAIL_COLOUR:
+                        print("failed to verify loading at: {} (x,y : {},{})".format(
+                            grayVersion[x, y], x, y))
+                    break
     return loading
