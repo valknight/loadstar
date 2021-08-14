12d8462d6482f46c8e3ab90a32695388bb9afd40
Val Knight <val@valknight.xyz>
2021-08-14 17:04:04 +0100

    Proof of concept switch to threading for running the cStar loop
    Signed-off-by: Val Knight <val@valknight.xyz>

--- src/loadstar/__main__.py
+++ src/loadstar/__main__.py
@@ -1,6 +1,22 @@
-from loadstar.cookstar import Cookstar
 
-if __name__ == '__main__':
-    c = Cookstar()
+from loadstar.cookstar import Cookstar
+from threading import Thread
+from datetime import datetime
+import time
+
+c = Cookstar()
+def cStarLoop():
     while True:
         c.loop()
+
+cookstar_loop = Thread(target=cStarLoop)
+
+def second():
+    while True:
+        print(datetime.now().strftime("%H:%M:%S"))
+        time.sleep(1)
+s = Thread(target=second)
+
+if __name__ == '__main__':
+    s.start()
+    cookstar_loop.start()
